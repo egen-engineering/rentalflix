3368d70888a2c0bcc0517cf45565fcdf8d91d8f2
Shreshth Kandari <shreshth.kandari@gmail.com>
2016-02-12 15:48:43 -0500

    feat(movie service) : implement movie service crud operation

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -5,5 +5,56 @@ package io.egen.rentalflix;
  * Fields: id, title, year, language
  */
 public class Movie {
-	//POJO IMPLEMENTATION GOES HERE
+    //POJO IMPLEMENTATION GOES HERE
+    private int id;
+    private String title;
+    private int year;
+    private String language;
+    private float rating;
+    private boolean rented;
+    private String rentedUser;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public float getRating() { return rating; }
+
+    public void setRating(float rating) { this.rating = rating; }
+
+    public boolean isRented() { return rented; }
+
+    public void setRented(boolean rented) { this.rented = rented; }
+
+    public String getRentedUser() { return rentedUser; }
+
+    public void setRentedUser(String rentedUser) { this.rentedUser = rentedUser; }
 }
--- src/main/java/io/egen/rentalflix/MovieService.java
+++ src/main/java/io/egen/rentalflix/MovieService.java
@@ -1,9 +1,84 @@
 package io.egen.rentalflix;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Service implementing IFlix interface
  * You can use any Java collection type to store movies
  */
 public class MovieService implements IFlix {
 
+    private List<Movie> movObj = Collections.synchronizedList(new ArrayList<Movie>());
+
+    @java.lang.Override
+    public List<String> findAll() {
+        List<String> stringMovieList = new ArrayList<String>();
+        for (Movie obj : movObj) {
+            stringMovieList.add(obj.getTitle());
+        }
+        return stringMovieList;
+    }
+
+    @java.lang.Override
+    public List<String> findByName(String name) {
+
+        List<String> stringMovieList = new ArrayList<String>();
+        for (Movie obj : movObj) {
+            if (name.equals(obj.getTitle())) stringMovieList.add(obj.getTitle());
+        }
+        return stringMovieList;
+    }
+
+    @java.lang.Override
+    public Movie create(Movie movie) {
+        movObj.add(movie);
+        return null;
+    }
+
+    @java.lang.Override
+    public Movie update(Movie movie) {
+        boolean flag = true;
+        for (int i = 0; i < movObj.size(); i++) {
+            if (movie.getId() == movObj.get(i).getId()) {
+                flag = false;
+                movObj.set(i, movie);
+            }
+        }
+        if (flag) throw new IllegalArgumentException("movie with this id is not found!");
+        else return movie;
+    }
+
+    @java.lang.Override
+    public Movie delete(int id) {
+        Movie deletedMovie;
+        for (int i = 0; i < movObj.size(); i++) {
+            if (movObj.get(i).getId() == id) {
+                deletedMovie = movObj.get(i);
+                movObj.remove(i);
+                return  deletedMovie;
+            }
+        }
+        throw new IllegalArgumentException("movie with this id is not found!");
+    }
+
+    @java.lang.Override
+    public boolean rentMovie(int movieId, String user) {
+
+        for (int i = 0; i < movObj.size(); i++) {
+            if (movObj.get(i).getId() == movieId) {
+                if(movObj.get(i).isRented())
+                {
+                    throw new IllegalArgumentException("movie with this id is already rented out!");
+                }
+                else{
+                    movObj.get(i).setRented(true);
+                    movObj.get(i).setRentedUser(user);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
