e86b79993bf51fd2497ab243c78436bee06fb913
surya <surya.mylar@gmail.com>
2016-02-11 20:19:48 -0500

    feat(Movie):created movie class

--- src/main/java/io/egen/rentalflix/MovieService.java
+++ src/main/java/io/egen/rentalflix/MovieService.java
@@ -1,9 +1,47 @@
 package io.egen.rentalflix;
 
+import java.util.List;
+
 /**
  * Service implementing IFlix interface
  * You can use any Java collection type to store movies
  */
 public class MovieService implements IFlix {
+
+	@Override
+	public List<Movie> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Movie> findByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Movie create(Movie movie) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Movie update(Movie movie) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Movie delete(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean rentMovie(int movieId, String user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
